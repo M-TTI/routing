36821e8ee44278fea9e2e36495d7e675504ba02d
Martin Bonetti <martin.bonetti@ynov.com>
2026-03-05 18:52:14 +0100

    working on skin importer service

---
+++ routing/lib/services/skin_importer.dart
@@ -0,0 +1,48 @@
+import 'dart:io';
+import 'package:file_picker/file_picker.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as p;
+import 'package:flutter_archive/flutter_archive.dart';
+
+class SkinImporter {
+  Future<List<File>> _selectFiles() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(allowMultiple: true, allowedExtensions: ['.osk', '.zip']);
+
+    if (result != null) {
+      List<File> files = result.paths.map((path) => File(path!)).toList();
+
+      return files;
+    }
+
+    return List.empty();
+  }
+
+  Future<bool> _extractFile(File file, Directory targetDir) async {
+    try {
+      ZipFile.extractToDirectory(zipFile: file, destinationDir: targetDir);
+    } catch (e) {
+      return false;
+    }
+
+    return true;
+  }
+
+  Future<bool> _saveFile(File file, Directory tmpDir) async {
+    // Moves the archive to the tmp dir
+    await file.rename(p.join(tmpDir.path, p.basename(file.path)));
+
+  }
+
+  Future<void> importFiles() async {
+    List<File> files = await _selectFiles();
+
+    if (files.isEmpty) {
+      return;
+    }
+
+    Directory tmpDir = await getTemporaryDirectory();
+    Directory tmpRoutingDir = await Directory(p.join(tmpDir.path, 'Routing')).createTemp();
+
+
+  }
+}
